6e9d57c573c835811002313f6a132d3043d2a4c4
Meng Li <jowett.lee@gmail.com>
2013-06-20 08:48:29 +0800

    Rotate Image

---
+++ Rotate Image.java
@@ -0,0 +1,27 @@
+You are given an n x n 2D matrix representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+Follow up:
+Could you do this in-place?
+
+public class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        if (n == 0) return;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int t = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = t;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int t = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = t;
+            }
+        }
+    }
+}
