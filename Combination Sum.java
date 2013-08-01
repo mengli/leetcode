213ef5e3c830b372607aee5e3b07a4d26b6b5033
Meng Li <jowett.lee@gmail.com>
2013-08-01 17:35:20 +0800

    some code

--- Combination Sum.java
+++ Combination Sum.java
@@ -1,3 +1,17 @@
+Given a set of candidate numbers (C) and a target number (T), find all unique combinations in C where the candidate numbers sums to T.
+
+The same repeated number may be chosen from C unlimited number of times.
+
+Note:
+
+All numbers (including target) will be positive integers.
+Elements in a combination (a1, a2, ? , ak) must be in non-descending order. (ie, a1 ? a2 ? ? ? ak).
+The solution set must not contain duplicate combinations.
+For example, given candidate set 2,3,6,7 and target 7, 
+A solution set is: 
+[7] 
+[2, 2, 3] 
+
 public class Solution {
 	public ArrayList<ArrayList<Integer>> combinationSum(int[] candidates, int target) {
 		ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
