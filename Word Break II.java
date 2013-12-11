ea06d52efbcc584c326321df62f2f4074828ba28
Meng Li <jowett.lee@gmail.com>
2013-12-11 14:39:44 +0800

    Word Break

--- Word Break II.java
+++ Word Break II.java
@@ -1,36 +1,56 @@
+Given a string s and a dictionary of words dict, add spaces in s to construct a sentence where each word is a valid dictionary word.
+
+Return all such possible sentences.
+
+For example, given
+s = "catsanddog",
+dict = ["cat", "cats", "and", "sand", "dog"].
+
+A solution is ["cats and dog", "cat sand dog"].
+
 public class Solution {
     public ArrayList<String> wordBreak(String s, Set<String> dict) {
-        Map<String, Boolean> wordMap = new HashMap<String, Boolean>();
+    	Map<String, Boolean> wordMap = new HashMap<String, Boolean>();
         for (String w : dict) {
         	wordMap.put(w, true);
         }
-        ArrayList<String> ret = new ArrayList<String>();
-        ArrayList<String> sol = new ArrayList<String>();
-        check(s, 0, wordMap, ret, sol);
-        return ret;
+        int len = s.length();
+        boolean[] strMap = new boolean[len + 1];
+        ArrayList<String> sols = new ArrayList<String>();
+        strMap[0] = true;
+        for (int i = 1; i <= len; i++) {
+        	for (int j = 0; j < i; j++) {
+        		String ww = s.substring(j, i);
+        		if (strMap[j] && wordMap.containsKey(ww)) {
+        			strMap[i] = true;
+        		}
+        	}
+        }
+        if (!strMap[len]) return sols;
+        List<String> sb = new LinkedList<String>();
+        search (s, dict, 0, len, strMap, sb, sols);
+        
+        return sols;
     }
     
-    private void check(String s, int i, Map<String, Boolean> wordMap, ArrayList<String> ret, ArrayList<String> sol) {
-    	int len = s.length();
-    	if (i >= len && sol.size() > 0) {
-    		StringBuffer sb = new StringBuffer();
-    		for (String ss : sol) {
-    			if (sb.length() > 0) {
-    				sb.append(" ");
-    			}
-    			sb.append(ss);
+    private void search(String s, Set<String> dict, int st, int len, boolean[] strMap, List<String> sb, ArrayList<String> sols) {
+    	if (st >= len) {
+    		StringBuffer sbf = new StringBuffer();
+    		for (String ss : sb) {
+    			sbf.append(" ");
+    			sbf.append(ss);
     		}
-    		ret.add(sb.toString());
+    		sols.add(sbf.toString().trim());
+    	} else {
+    		for (int ll = st + 1; ll <= len; ll++) {
+    			String tmp = s.substring(st, ll);
+	    		if (strMap[st] && dict.contains(tmp)) {
+	    			sb.add(tmp);
+	    			int last = sb.size() - 1;
+	    			search(s, dict, ll, len, strMap, sb, sols);
+	    			sb.remove(last);
+	    		}
+    		}
     	}
-    	int j = i + 1;
-    	while(j <= len) {
-			String word = s.substring(i, j);
-			if (wordMap.containsKey(word)) {
-				sol.add(word);
-				check(s, j, wordMap, ret, sol);
-				sol.remove(sol.size() - 1);
-			}
-			j++;
-		}
 	}
 }
