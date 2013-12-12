a6fa19f9a614c1ef9acbea0a01c5ec8f02dc6b13
Meng Li <jowett.lee@gmail.com>
2013-12-12 12:49:28 +0800

    Reorder List

--- src/leetcode/ReorderList.java
+++ src/leetcode/ReorderList.java
@@ -1,35 +1,71 @@
 package leetcode;
 
 /**
+ * Given a singly linked list L: L0->L1->...->Ln-1->Ln,
+ * reorder it to: L0->Ln->L1->Ln-1->L2->Ln-2->...
+ * 
+ * You must do this in-place without altering the nodes' values.
+ * 
+ * For example, Given {1,2,3,4}, reorder it to {1,4,2,3}.
+ * 
+ */
+
+/**
  * Definition for singly-linked list. class ListNode { int val; ListNode next;
  * ListNode(int x) { val = x; next = null; } }
  */
 public class ReorderList {
 	public void reorderList(ListNode head) {
-		if (head == null || head.next == null || head.next.next == null) return;
-		ListNode cur = head;
-		ListNode prev = null;
-		while(cur.next != null) {
-			prev = cur;
+		if (head == null || head.next == null)
+			return;
+		ListNode fast = head;
+		ListNode late = head;
+		while (fast.next != null && fast.next.next != null) {
+			fast = fast.next.next;
+			late = late.next;
+		}
+		ListNode ret = new ListNode(0);
+		ListNode cur = ret;
+		ListNode leftHalf = head;
+		ListNode rightHalf;
+		if (fast.next != null) {
+			rightHalf = reverseList(late.next);
+			late.next = null;
+		} else {
+			rightHalf = reverseList(late);
+			ListNode tmp = head;
+			while (tmp.next != late) {
+				tmp = tmp.next;
+			}
+			tmp.next = null;
+		}
+		leftHalf = head;
+		while (leftHalf != null && rightHalf != null) {
+			cur.next = leftHalf;
+			leftHalf = leftHalf.next;
+			cur = cur.next;
+			cur.next = rightHalf;
+			rightHalf = rightHalf.next;
 			cur = cur.next;
 		}
-		ListNode next = head.next;
-		head.next = cur;
-		prev.next = cur.next;
-		cur.next = next;
-		reorderList(next);
+		if (leftHalf != null) {
+			cur.next = leftHalf;
+		} else if (rightHalf != null) {
+			cur.next = rightHalf;
+		}
+		head = ret.next;
 	}
-	
-	public static void main(String[] args) {
-		ListNode head0 = new ListNode(0);
-		ListNode head1 = new ListNode(1);
-		ListNode head2 = new ListNode(2);
-		ListNode head3 = new ListNode(3);
-		//ListNode head4 = new ListNode(4);
-		head0.next = head1;
-		head1.next = head2;
-		head2.next = head3;
-		//head3.next = head4;
-		new ReorderList().reorderList(head0);
+
+	private ListNode reverseList(ListNode head) {
+		ListNode cur = head;
+		ListNode prev = null;
+		ListNode next = null;
+		while (cur != null) {
+			next = cur.next;
+			cur.next = prev;
+			prev = cur;
+			cur = next;
+		}
+		return prev;
 	}
 }
