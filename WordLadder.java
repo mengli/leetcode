4fa9304f21092d2f01bca372d6c6f5eb8232fb00
Meng Li <jowett.lee@gmail.com>
2013-12-18 17:05:22 +0800

    Word Ladder

--- WordLadder.java
+++ WordLadder.java
@@ -24,44 +24,56 @@ import java.util.Queue;
 
 public class WordLadder {
 	public int ladderLength(String start, String end, HashSet<String> dict) {
-		if (dict.isEmpty()) {
+		if (dict.size() == 0)
 			return 0;
-		}
-		int currentLevel = 1, nextLevel = 0;
-		int steps = 0;
+		int currentLevel = 1;
+		int nextLevel = 0;
+		int step = 1;
 		boolean found = false;
+		Queue<String> st = new LinkedList<String>();
 		HashSet<String> visited = new HashSet<String>();
-
-		Queue<String> q = new LinkedList<String>();
-		q.add(start);
-		while (!q.isEmpty()) {
-			String st = q.remove();
+		st.add(start);
+		while (!st.isEmpty()) {
+			String s = st.remove();
 			currentLevel--;
-			if (st.equals(end)) {
-				steps++;
+			if (stringDiff(s, end) == 1) {
 				found = true;
+				step++;
 				break;
 			} else {
-				for (int i = 0; i < st.length(); i++) {
-					StringBuffer sb = new StringBuffer(st);
+				int length = s.length();
+				StringBuffer sb = new StringBuffer(s);
+				for (int i = 0; i < length; i++) {
 					for (int j = 0; j < 26; j++) {
+						char c = sb.charAt(i);
 						sb.setCharAt(i, (char) ('a' + j));
-						String next = sb.toString();
-						if (dict.contains(next) && !visited.contains(next)) {
-							q.add(next);
-							visited.add(next);
+						if (dict.contains(sb.toString())
+								&& !visited.contains(sb.toString())) {
 							nextLevel++;
+							st.add(sb.toString());
+							visited.add(sb.toString());
 						}
+						sb.setCharAt(i, c);
 					}
 				}
 			}
 			if (currentLevel == 0) {
-				steps++;
 				currentLevel = nextLevel;
 				nextLevel = 0;
+				step++;
 			}
 		}
+		return found ? step : 0;
+	}
 
-		return found ? steps : 0;
+	private int stringDiff(String s1, String s2) {
+		int diff = 0;
+		int length = s1.length();
+		for (int i = 0; i < length; i++) {
+			if (s1.charAt(i) != s2.charAt(i)) {
+				diff++;
+			}
+		}
+		return diff;
 	}
 }
