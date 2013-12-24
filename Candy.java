407eb405cd968916d9a746ada9c7066a76458d64
Li Meng <limeng@li-mengdemacbook.local>
2013-12-24 11:35:02 +0800

    Candy

--- Candy.java
+++ Candy.java
@@ -1,5 +1,4 @@
 
-
 /**
  * There are N children standing in a line. Each child is assigned a rating
  * value. You are giving candies to these children subjected to the following
@@ -14,32 +13,16 @@
 
 public class Candy {
 	public int candy(int[] ratings) {
-		int[] A = new int[ratings.length];
-		for (int i = 0; i < A.length; i++) {
-			A[i] = 1;
+		int[] candy = new int[ratings.length];
+		candy[0] = 1;
+		for (int i = 1; i < ratings.length; i++) {
+			candy[i] = ratings[i] > ratings[i - 1] ? candy[i - 1] + 1 : 1;
 		}
-		for (int j = 1; j < A.length; j++) {
-			check(A, ratings, j);
+		int totalCandy = candy[ratings.length - 1];
+		for (int i = ratings.length - 2; i >= 0; i--) {
+			candy[i] = (ratings[i] > ratings[i + 1] && candy[i + 1] + 1 > candy[i]) ? candy[i + 1] + 1 : candy[i];
+			totalCandy += candy[i];
 		}
-		int sum = 0;
-		for (int k = 0; k < A.length; k++) {
-			sum += A[k];
-		}
-		return sum;
-	}
-
-	private void check(int[] plan, int[] ratings, int start) {
-		while (start > 0) {
-			if (ratings[start - 1] > ratings[start]
-					&& plan[start - 1] <= plan[start]) {
-				plan[start - 1] = plan[start] + 1;
-			} else if (ratings[start - 1] < ratings[start]
-					&& plan[start - 1] >= plan[start]) {
-				plan[start] = plan[start - 1] + 1;
-			} else {
-				return;
-			}
-			start--;
-		}
+		return totalCandy;
 	}
 }
