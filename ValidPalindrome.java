a04d290390a705280b9f82072fbb83203f12a066
Li Meng <limeng@li-mengdemacbook.local>
2013-12-25 17:11:03 +0800

    refine

--- ValidPalindrome.java
+++ ValidPalindrome.java
@@ -1,5 +1,4 @@
 
-
 /**
  * Given a string, determine if it is a palindrome, considering only
  * alphanumeric characters and ignoring cases.
@@ -7,44 +6,33 @@
  * For example, "A man, a plan, a canal: Panama" is a palindrome. "race a car"
  * is not a palindrome.
  * 
- * Note: 
+ * Note:
  * 
- * Have you consider that the string might be empty? This is a good question to ask during an interview.
+ * Have you consider that the string might be empty? This is a good question to
+ * ask during an interview.
  * 
  * For the purpose of this problem, we define empty string as valid palindrome.
  */
 
 public class ValidPalindrome {
 	public boolean isPalindrome(String s) {
-		s = s.toLowerCase();
-		if (s.length() == 0)
-			return true;
-		else {
-			int begin = 0;
-			int end = s.length() - 1;
-			while (true) {
-				while (end > 0
-						&& !((s.charAt(end) >= 'a' && s.charAt(end) <= 'z')
-								|| (s.charAt(end) >= 'A' && s.charAt(end) <= 'Z') || ((s
-								.charAt(end) >= '0' && s.charAt(end) <= '9')))) {
-					end--;
-				}
-				while (begin < s.length()
-						&& !((s.charAt(begin) >= 'a' && s.charAt(begin) <= 'z') || (s
-								.charAt(begin) >= 'A' && s.charAt(begin) <= 'Z' || ((s
-								.charAt(begin) >= '0' && s.charAt(begin) <= '9'))))) {
-					begin++;
-				}
-				if (end - begin > 0) {
-					if (s.charAt(end) != s.charAt(begin)) {
-						return false;
-					} else {
-						end--;
-						begin++;
-					}
-				} else {
-					return true;
-				}
+		s = s.toUpperCase();
+		int i = 0, j = s.length() - 1;
+		while (true) {
+			while (i < s.length()
+					&& !(s.charAt(i) >= 'A' && s.charAt(i) <= 'Z')
+					&& !(s.charAt(i) >= '0' && s.charAt(i) <= '9'))
+				i++;
+			while (j >= 0 && !(s.charAt(j) >= 'A' && s.charAt(j) <= 'Z')
+					&& !(s.charAt(j) >= '0' && s.charAt(j) <= '9'))
+				j--;
+			if (i >= j) {
+				return true;
+			} else if (s.charAt(i) != s.charAt(j)) {
+				return false;
+			} else {
+				i++;
+				j--;
 			}
 		}
 	}
