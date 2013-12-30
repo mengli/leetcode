eaeeacf0d76e6c302f2460e4369f2e21475fea38
Meng Li <jowett.lee@gmail.com>
2013-12-30 14:17:17 +0800

    refine

--- BinaryTreeMaximumPathSum.java
+++ BinaryTreeMaximumPathSum.java
@@ -1,5 +1,3 @@
-
-
 /**
  * Given a binary tree, find the maximum path sum.
  * 
@@ -16,40 +14,37 @@
  */
 
 public class BinaryTreeMaximumPathSum {
-
-	public class MyInt {
-
-		private int value;
-
-		public int getValue() {
-			return value;
-		}
-
-		public void setValue(int value) {
-			this.value = value;
-		}
-
-		public MyInt(int value) {
-			this.value = value;
-		}
-
-	}
-
 	public int maxPathSum(TreeNode root) {
-		MyInt maxSum = new MyInt(Integer.MIN_VALUE);
-		findMaxSum(root, maxSum);
-		return maxSum.getValue();
-	}
-
-	public int findMaxSum(TreeNode root, MyInt maxSum) {
 		if (root == null)
 			return 0;
-		int cpath = 0;
-		int left = findMaxSum(root.left, maxSum);
-		int right = findMaxSum(root.right, maxSum);
-		cpath = Math.max(root.val, Math.max(root.val + left, root.val + right));
-		maxSum.setValue(Math.max(maxSum.getValue(),
-				Math.max(root.val + left + right, cpath)));
-		return cpath;
+		int[] max = new int[1];
+		max[0] = Integer.MIN_VALUE;
+		maxPathSum(root, max);
+		return max[0];
+	}
+
+	private int maxPathSum(TreeNode root, int[] max) {
+		if (root.left == null && root.right == null) {
+			max[0] = root.val > max[0] ? root.val : max[0];
+			return root.val;
+		} else if (root.left != null && root.right == null) {
+			int left = maxPathSum(root.left, max);
+			max[0] = left > 0 ? Math.max(left + root.val, max[0]) : Math.max(
+					root.val, max[0]);
+			return left > 0 ? left + root.val : root.val;
+		} else if (root.left == null && root.right != null) {
+			int right = maxPathSum(root.right, max);
+			max[0] = right > 0 ? Math.max(right + root.val, max[0]) : Math.max(
+					root.val, max[0]);
+			return right > 0 ? right + root.val : root.val;
+		} else {
+			int left = maxPathSum(root.left, max);
+			int right = maxPathSum(root.right, max);
+			int tmp = root.val;
+			tmp += left > 0 ? left : 0;
+			tmp += right > 0 ? right : 0;
+			max[0] = tmp > max[0] ? tmp : max[0];
+			return Math.max(Math.max(left, right), 0) + root.val;
+		}
 	}
 }
--- SurroundedRegions.java
+++ SurroundedRegions.java
@@ -23,45 +23,50 @@ import java.util.Queue;
  */
 
 public class SurroundedRegions {
-
 	public void solve(char[][] board) {
-		int height = board.length;
-		int width = 0;
-		if (height > 0) {
-			width = board[0].length;
-		}
-
-		Queue<Point> q = new LinkedList<Point>();
-		for (int i = 0; i < width; i++) {
-			for (int j = 0; j < height; j++) {
-				if ((i == 0 || i == width - 1 || j == 0 || j == height - 1)
+		int row = board.length;
+		if (row == 0)
+			return;
+		int col = board[0].length;
+		Queue<Integer> qx = new LinkedList<Integer>();
+		Queue<Integer> qy = new LinkedList<Integer>();
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				if ((i == 0 || i == row - 1 || j == 0 || j == col - 1)
 						&& board[i][j] == 'O') {
-					q.add(new Point(i, j));
+					qx.add(i);
+					qy.add(j);
 				}
 			}
 		}
-
-		while (q.peek() != null) {
-			Point p = q.remove();
-			board[p.x][p.y] = '-';
-			if (p.x > 0 && board[p.x - 1][p.y] == 'O') {
-				q.add(new Point(p.x - 1, p.y));
+		while (!qx.isEmpty()) {
+			int x = qx.remove();
+			int y = qy.remove();
+			board[x][y] = '@';
+			if (x > 0 && board[x - 1][y] == 'O') {
+				qx.add(x - 1);
+				qy.add(y);
 			}
-			if (p.x < width - 1 && board[p.x + 1][p.y] == 'O') {
-				q.add(new Point(p.x + 1, p.y));
+			if (x < row - 1 && board[x + 1][y] == 'O') {
+				qx.add(x + 1);
+				qy.add(y);
 			}
-			if (p.y > 0 && board[p.x][p.y - 1] == 'O') {
-				q.add(new Point(p.x, p.y - 1));
+			if (y > 0 && board[x][y - 1] == 'O') {
+				qx.add(x);
+				qy.add(y - 1);
 			}
-			if (p.y < height - 1 && board[p.x][p.y + 1] == 'O') {
-				q.add(new Point(p.x, p.y + 1));
+			if (y < col - 1 && board[x][y + 1] == 'O') {
+				qx.add(x);
+				qy.add(y + 1);
 			}
 		}
-
-		for (int i = 0; i < width; i++) {
-			for (int j = 0; j < height; j++) {
-				if (board[i][j] != 'X')
-					board[i][j] = board[i][j] == 'O' ? 'X' : 'O';
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				if (board[i][j] == '@') {
+					board[i][j] = 'O';
+				} else {
+					board[i][j] = 'X';
+				}
 			}
 		}
 	}
