a3cfa6d7535187b5f8ea604942b029c957a95c19
Meng Li <jowett.lee@gmail.com>
2013-12-31 14:35:16 +0800

    refine

--- ConvertSortedListtoBinarySearchTree.java
+++ ConvertSortedListtoBinarySearchTree.java
@@ -1,31 +1,31 @@
 
-
 /**
- * Given a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+ * Given a singly linked list where elements are sorted in ascending order,
+ * convert it to a height balanced BST.
  */
 
 public class ConvertSortedListtoBinarySearchTree {
 	public TreeNode sortedListToBST(ListNode head) {
-        return sortedListToBST(head, null);
-    }
-    
-    private TreeNode sortedListToBST(ListNode start, ListNode end) {
-        if (start == end) {
-            return null;
-        } else if (start.next == end) {
-            return new TreeNode(start.val);
-        } else {
-            ListNode fast = start, slow = start;
-            while (fast.next != end && fast.next.next != end) {
-                fast = fast.next.next;
-                slow = slow.next;
-            }
-            TreeNode left = sortedListToBST(start, slow);
-            TreeNode right = sortedListToBST(slow.next, end);
-            TreeNode root = new TreeNode(slow.val);
-            root.left = left;
-            root.right = right;
-            return root;
-        }
-    }
+		return sortedListToBST(head, null);
+	}
+
+	private TreeNode sortedListToBST(ListNode start, ListNode end) {
+		if (start == end) {
+			return null;
+		} else if (start.next == end) {
+			return new TreeNode(start.val);
+		} else {
+			ListNode fast = start, slow = start;
+			while (fast.next != end && fast.next.next != end) {
+				fast = fast.next.next;
+				slow = slow.next;
+			}
+			TreeNode left = sortedListToBST(start, slow);
+			TreeNode right = sortedListToBST(slow.next, end);
+			TreeNode root = new TreeNode(slow.val);
+			root.left = left;
+			root.right = right;
+			return root;
+		}
+	}
 }
