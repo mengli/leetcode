2e4c5726d898243ec97dd37eaf97de40f7c19a0c
Meng Li <jowett.lee@gmail.com>
2013-12-31 14:30:27 +0800

    refine

--- ConvertSortedListtoBinarySearchTree.java
+++ ConvertSortedListtoBinarySearchTree.java
@@ -6,36 +6,26 @@
 
 public class ConvertSortedListtoBinarySearchTree {
 	public TreeNode sortedListToBST(ListNode head) {
-		ListNode n = head;
-		int size = 0;
-		while (n != null) {
-			size++;
-			n = n.next;
-		}
-		int[] num = new int[size];
-		n = head;
-		int i = 0;
-		while (n != null) {
-			num[i++] = n.val;
-			n = n.next;
-		}
-		return sortedArrayToBST(num);
-	}
-
-	public TreeNode sortedArrayToBST(int[] num) {
-		return sortedArrayToBST(num, 0, num.length - 1);
-	}
-
-	public TreeNode sortedArrayToBST(int[] num, int start, int end) {
-		if (start <= end) {
-			int mid = (start + end) / 2;
-			TreeNode left = sortedArrayToBST(num, start, mid - 1);
-			TreeNode right = sortedArrayToBST(num, mid + 1, end);
-			TreeNode node = new TreeNode(num[mid]);
-			node.left = left;
-			node.right = right;
-			return node;
-		}
-		return null;
-	}
+        return sortedListToBST(head, null);
+    }
+    
+    private TreeNode sortedListToBST(ListNode start, ListNode end) {
+        if (start == end) {
+            return null;
+        } else if (start.next == end) {
+            return new TreeNode(start.val);
+        } else {
+            ListNode fast = start, slow = start;
+            while (fast.next != end && fast.next.next != end) {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+            TreeNode left = sortedListToBST(start, slow);
+            TreeNode right = sortedListToBST(slow.next, end);
+            TreeNode root = new TreeNode(slow.val);
+            root.left = left;
+            root.right = right;
+            return root;
+        }
+    }
 }
