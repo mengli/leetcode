6aab717f5496b71e5f4e12a4bda985cc5bc87d21
Meng Li <jowett.lee@gmail.com>
2014-01-03 08:26:03 +0800

    refine

--- SymmetricTree.java
+++ SymmetricTree.java
@@ -1,6 +1,4 @@
-
-
-import java.util.ArrayList;
+import java.util.Stack;
 
 /**
  * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
@@ -25,27 +23,28 @@ import java.util.ArrayList;
 
 public class SymmetricTree {
 	public boolean isSymmetric(TreeNode root) {
-		ArrayList<String> inOrder = new ArrayList<String>();
-		inOrder(root, inOrder);
-		int start = 0, end = inOrder.size() - 1;
-		while (start < end) {
-			if (inOrder.get(start).equals(inOrder.get(end))) {
-				start++;
-				end--;
-			} else {
+		if (root == null)
+			return true;
+		Stack<TreeNode> s1 = new Stack<TreeNode>();
+		Stack<TreeNode> s2 = new Stack<TreeNode>();
+		s1.push(root.left);
+		s2.push(root.right);
+		while (!s1.isEmpty() && !s2.isEmpty()) {
+			TreeNode n1 = s1.pop();
+			TreeNode n2 = s2.pop();
+			if (n1 == null && n2 == null) {
+				continue;
+			} else if (n1 == null || n2 == null) {
 				return false;
+			} else if (n1.val != n2.val) {
+				return false;
+			} else {
+				s1.push(n1.left);
+				s1.push(n1.right);
+				s2.push(n2.right);
+				s2.push(n2.left);
 			}
 		}
 		return true;
-	}
-
-	public void inOrder(TreeNode root, ArrayList<String> inOrder) {
-		if (root == null) {
-			inOrder.add("#");
-			return;
-		}
-		inOrder(root.left, inOrder);
-		inOrder.add(root.val + "");
-		inOrder(root.right, inOrder);
 	}
 }
