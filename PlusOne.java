6eb686ade9eaf7b6672ede3c7b5a314c632c4efb
Meng Li <jowett.lee@gmail.com>
2014-01-07 11:12:39 +0800

    refine

--- PlusOne.java
+++ PlusOne.java
@@ -1,32 +1,23 @@
-
-
 /** 
  * Given a number represented as an array of digits, plus one to the number. 
  */
 
 public class PlusOne {
 	public int[] plusOne(int[] digits) {
-		int advance = 0;
-		int i = digits.length - 1;
-		digits[i] += 1;
-		do {
-			digits[i] = digits[i] + advance;
-			if (digits[i] >= 10) {
-				digits[i] -= 10;
-				advance = 1;
-			} else {
-				advance = 0;
-			}
-			i--;
-		} while (advance != 0 && i >= 0);
-		if (advance == 1) {
-			int[] result = new int[digits.length + 1];
-			result[0] = 1;
-			for (int j = 0; j < digits.length; j++) {
-				result[j + 1] = digits[j];
-			}
-			return result;
+		int length = digits.length;
+		int add = 1;
+		for (int i = length - 1; i >= 0; i--) {
+			int sum = digits[i] + add;
+			digits[i] = sum % 10;
+			add = sum / 10;
+			if (add == 0)
+				return digits;
 		}
-		return digits;
+		int[] ret = new int[length + 1];
+		ret[0] = add;
+		for (int i = 0; i < length; i++) {
+			ret[i + 1] = digits[i];
+		}
+		return ret;
 	}
 }
