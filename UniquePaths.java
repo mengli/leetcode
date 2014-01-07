c00c4cf491ec44a55bb877d738ef1c9c0f841eb3
Meng Li <jowett.lee@gmail.com>
2014-01-07 13:15:06 +0800

    Unique Paths

---
+++ UniquePaths.java
@@ -0,0 +1,35 @@
+/**
+ * A robot is located at the top-left corner of a m x n grid (marked 'Start' in
+ * the diagram below).
+ * 
+ * The robot can only move either down or right at any point in time. The robot
+ * is trying to reach the bottom-right corner of the grid (marked 'Finish' in
+ * the diagram below).
+ * 
+ * How many possible unique paths are there?
+ * 
+ * Above is a 3 x 7 grid. How many possible unique paths are there?
+ * 
+ * Note: m and n will be at most 100.
+ * 
+ */
+
+public class UniquePaths {
+	public int uniquePaths(int m, int n) {
+		if (m == 0 || n == 0)
+			return 0;
+		int[][] map = new int[m][n];
+		for (int i = 0; i < m; i++) {
+			map[i][0] = 1;
+		}
+		for (int j = 0; j < n; j++) {
+			map[0][j] = 1;
+		}
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				map[i][j] = map[i - 1][j] + map[i][j - 1];
+			}
+		}
+		return map[m - 1][n - 1];
+	}
+}
